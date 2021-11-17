3b6c2a62a7e4c793b0a62724d4d754e9a75894a7
arT2 <60965610+etrusci-org@users.noreply.github.com>
2021-11-17 18:22:15 +0100

    Update data.js

--- data.js
+++ data.js
@@ -9,4 +9,12 @@ APP.videos = [
     "rdlvPe959Ck",
     "3FwQzeG7hoM",
     "TDE0EatumPg",
+    "bx7Yd39DShg",
+    "rQtChq8GARw",
+    "yURRmWtbTbo",
+    "_gmmvFBGkjc",
+    "ZkivTBAMuaE",
+    "F-TYRhBHbwc",
+    "sNUSBi8x4tg",
+    "R8AOAap6_k4",
 ]
